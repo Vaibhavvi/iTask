5adf42f9f0fb2e76090a8719040e681ee559e787
Vaibhavvi <163631801+Vaibhavvi@users.noreply.github.com>
2025-01-20 13:21:29 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/iTask/', // Change this to your project name if you're deploying to GitHub Pages
 })
